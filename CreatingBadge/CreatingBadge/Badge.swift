44ec385715e0c6dc1b896ae56aeb61ced2da86e2
Rui Cao <rayray199085@gmail.com>
2019-10-14 14:30:35 +1100

    Use path to draw patterns

---
+++ CreatingBadge/CreatingBadge/Badge.swift
@@ -0,0 +1,40 @@
+//
+//  Badge.swift
+//  CreatingBadge
+//
+//  Created by Stephen Cao on 14/10/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    static let rotationCount = 8
+    
+    var badgeSymbols: some View {
+        ForEach(0..<Badge.rotationCount) { i in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(i) / Double(Badge.rotationCount)) * 360.0
+            )
+        }
+        .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                self.badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }.scaledToFit()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
---
+++ CreatingBadge/CreatingBadge/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  CreatingBadge
+//
+//  Created by Stephen Cao on 14/10/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+        .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
